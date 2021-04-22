e7a9080128d5af3e36c34c1d15b02461fd741f9b
auxb <auxence.blondel@gmail.com>
2021-04-22 11:16:16 +0200

    init apollo and use it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
+import {
+  gql,
+  useQuery,
+} from "@apollo/client";
 import logo from './logo.svg';
 import './App.css';
+
+const LAUNCHES_DATA = gql`
+  query {
+    launches(limit: 5) {
+      id
+      launch_date_utc
+      launch_success
+      rocket {
+        rocket_name
+      }
+      links {
+        video_link
+      }
+      details
+    }
+  }
+`;
+
+function LaunchsData() {
+  const { loading, error, data } = useQuery(LAUNCHES_DATA);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+    console.log(data.launches)
+  return data.launches.map((launch) => (
+    < div key = { launch.id } >
+      <h2>{launch.rocket['rocket_name']}</h2>
+      <p>
+        Mission name: {launch["mission_name"]}
+      </p>
+      <p>
+        Launch date: {launch["launch_date_utc"]}
+      </p>
+      <p>
+        Details {launch.details}
+      </p>
+      <p>
+        Launch success: {launch["launch_success"]}
+      </p>
+    </div >
+  ));
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <LaunchsData />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
